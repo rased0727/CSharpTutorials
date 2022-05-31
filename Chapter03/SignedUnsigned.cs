c031f3c0b8f46ebc6ed4a193a1b851a10d555b89
Rased Jung <rased0727@gmail.com>
2022-05-31 13:36:04 +0900

    220531 1336

---
+++ Chapter03/SignedUnsigned.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter03
+{
+    class SignedUnsigned
+    {
+        static void Main(string[] args)
+        {
+            byte a = 255;
+            sbyte b = (sbyte)a;
+
+            Console.WriteLine($"a={a}");
+            Console.WriteLine($"b={b}");
+
+        }
+    }
+}
