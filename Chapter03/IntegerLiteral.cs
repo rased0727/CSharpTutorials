728700cd4c8f402fba3563f6a0b834566a8b0857
Rased Jung <rased0727@gmail.com>
2022-05-31 13:18:32 +0900

    220531 1318

---
+++ Chapter03/IntegerLiteral.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter03
+{
+    class IntegerLiteral
+    {
+        static void Main(string[] args)
+        {
+            byte a = 240;
+            Console.WriteLine($"a={a}");
+
+            byte b = 0b1111_0000;
+            Console.WriteLine($"b={b}");
+
+            byte c = 0xF0;
+            Console.WriteLine($"c={c}");
+
+            uint d = 0x1234_abcd;
+            Console.WriteLine($"d={d}");
+
+
+
+
+
+        }
+    }
+}
---
+++ Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+            
+            Console.WriteLine("Hello Unity! {0}", args[0]);
+        }
+    }
+}
