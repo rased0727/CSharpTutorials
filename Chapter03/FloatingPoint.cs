dfb5a62b6e1545915450d41cb2b286079d431eb1
Rased Jung <rased0727@gmail.com>
2022-06-02 13:45:06 +0900

    220602 1345

---
+++ Chapter03/FloatingPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter03
+{
+    class FloatingPoint
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.14159265358979323846f;
+            Console.WriteLine(a);
+
+            double b = 3.14159265358979323846;
+            Console.WriteLine(b);
+
+            decimal c = 3.141592653589793238462643383279m;
+            Console.WriteLine(c);
+        }
+    }
+}
