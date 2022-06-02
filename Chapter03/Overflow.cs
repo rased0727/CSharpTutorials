8b189703492f678778d34c910f006ebcf68a9405
Rased Jung <rased0727@gmail.com>
2022-06-02 13:08:55 +0900

    220602 1308

---
+++ Chapter03/Overflow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter03
+{
+    class Overflow
+    {
+        static void Main(string[] args)
+        {
+            uint a = uint.MaxValue;
+
+            Console.WriteLine(a);
+            a = a + 1;
+            Console.WriteLine(a);
+        }
+    }
+}
