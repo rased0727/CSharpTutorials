5663f99d6cb2a5ba1aaa6ff77be0a480ac429e10
Rased Jung <rased0727@gmail.com>
2022-06-07 15:45:47 +0900

    220607 1545

---
+++ Chapter03/BoxingUnBoxing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter03
+{
+    class BoxingUnBoxing
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            object b = (object)a;
+            int c = (int)b;
+
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            double x = 3.1414213;
+            object y = x;
+            double z = (double)y;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+        }
+    }
+}
