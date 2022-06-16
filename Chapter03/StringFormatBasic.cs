4dbdc72e39d60ceae58ee3d1b3d22d176b404717
Rased Jung <rased0727@gmail.com>
2022-06-16 15:52:15 +0900

    220616 1552

---
+++ Chapter03/StringFormatBasic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter03
+{
+    class StringFormatBasic
+    {
+        static void Main(string[] args)
+        {
+            string fmt = "{0, -20}{1, -15}{2, 30}";
+
+            WriteLine(fmt, "Publisher", "Author", "Title");
+            WriteLine(fmt, "Marvel", "Stan Lee", "Irom Man");
+            WriteLine(fmt, "Hanbit", "SangHyun Park", "This is C#");
+            WriteLine(fmt, "Prentice Hall", "K&R", "The C Programming Language");
+
+        }
+    }
+}
