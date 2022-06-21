e6a7f278d0100634c68bbc62b7a78ab70ab9886f
Rased Jung <rased0727@gmail.com>
2022-06-21 15:38:31 +0900

    220621 1538

---
+++ Chapter04/Chapter04/Chapter04/StringConcatenate_RelationalOperater.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter04
+{
+    class StringConcatenate_RelationalOperater
+    {
+        static void Main(string[] args)
+        {
+            string result = "123" + "456";
+            Console.WriteLine(result);
+
+            result = "Hello" + " " + "World";
+            Console.WriteLine(result);
+
+            Console.WriteLine();
+
+
+            Console.WriteLine($"3>4 : { 3 > 4}");
+            Console.WriteLine($"3>=4 : {3 >= 4}");
+            Console.WriteLine($"3<4 : {3 < 4}");
+            Console.WriteLine($"3<=4 : {3 <= 4}");
+            Console.WriteLine($"3==4: {3 == 4}");
+            Console.WriteLine($"3!=4: {3 != 4}");
+
+
+        }
+    }
+}
