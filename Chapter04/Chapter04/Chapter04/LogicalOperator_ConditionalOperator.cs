5f3ad7b32aa8ffbdbb6c64f0e0a4e62be16fb23a
Rased Jung <rased0727@gmail.com>
2022-06-22 15:20:44 +0900

    220622 1520

---
+++ Chapter04/Chapter04/Chapter04/LogicalOperator_ConditionalOperator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter04
+{
+    class LogicalOperator_ConditionalOperator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing && ...");
+            Console.WriteLine($"1>0 && 4<5 : {1 > 0 && 4 < 5}");
+            Console.WriteLine($"1>0 && 4>5 : {1 > 0 && 4 > 5}");
+            Console.WriteLine($"1==0 && 4>5 : {1 == 0 && 4 > 5}");
+            Console.WriteLine($"1==0 && 4<5 : {1 == 0 && 4 < 5}");
+
+            Console.WriteLine("\nTesting || ...");
+            Console.WriteLine($"1>0 || 4<5 : {1 > 0 || 4 < 5}");
+            Console.WriteLine($"1>0 || 4>5 : {1 > 0 || 4 > 5}");
+            Console.WriteLine($"1==0 || 4>5 : {1 == 0 || 4 > 5}");
+            Console.WriteLine($"1==0 || 4<5 : {1 == 0 || 4 < 5}");
+
+            Console.WriteLine("\nTesting ! ...");
+            Console.WriteLine($"!True : {!true}");
+            Console.WriteLine($"!False : {!false}");
+
+
+            string result = (10 % 2) == 0 ? "짝수" : "홀수";
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Chapter04/Chapter04/Chapter04/NullConditionalOperator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter04
+{
+    class NullConditionalOperator
+    {
+        static void Main(string[] args)
+        {
+            List<string> a = null;
+            a?.Add("야구");
+            a?.Add("축구");
+            Console.WriteLine($"Count : {a?.Count}");
+            Console.WriteLine($"{a?[0]}");
+            Console.WriteLine($"{a?[1]}");
+
+            a = new List<string>();
+
+            a?.Add("야구");
+            a?.Add("축구");
+
+            Console.WriteLine($"Count : {a?.Count}");
+            Console.WriteLine($"{a?[0]}");
+            Console.WriteLine($"{a?[1]}");
+
+        }
+    }
+}
