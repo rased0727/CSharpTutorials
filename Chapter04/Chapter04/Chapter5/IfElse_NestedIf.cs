97dc3585289f55f97f13f92ee7bc10a0c9582bbc
Rased Jung <rased0727@gmail.com>
2022-07-05 15:38:54 +0900

    220705 1538

---
+++ Chapter04/Chapter04/Chapter5/IfElse_NestedIf.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter5
+{
+    class IfElse_NestedIf
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("숫자를 입력하세요 : ");
+
+                string input = Console.ReadLine();
+                int number = Int32.Parse(input);
+
+                if (number < 0)
+                    Console.WriteLine("음수");
+                else if (number > 0)
+                    Console.WriteLine("양수");
+                else
+                    Console.WriteLine("0");
+
+                if (number % 2 == 0)
+                    Console.WriteLine("짝수");
+                else
+                    Console.WriteLine("홀수");
+
+            }
+            {
+                Console.Write("숫자를 입력하세요 : ");
+
+                string input = Console.ReadLine();
+                int number = Convert.ToInt32(input);
+
+                if (number > 0)
+                {
+                    if (number % 2 == 0)
+                        Console.WriteLine("0보다 큰 짝수.");
+                    else
+                        Console.WriteLine("0보다 큰 홀수.");
+                }
+                else
+                {
+                    Console.WriteLine("0보다 작거나 같은 수.");
+                }
+            }
+        }
+    }
+}
---
+++ Chapter04/Chapter04/Chapter5/Switch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter5
+{
+    class Switch
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("요일을 입력하세요. (일,월,화,수,목,금,토) : ");
+            string day = Console.ReadLine();
+
+            switch (day)
+            {
+                case "sun":
+                    Console.WriteLine("Sunday");
+                    break;
+                case "mon":
+                    Console.WriteLine("Monday");
+                    break;
+                case "tue":
+                    Console.WriteLine("Tuesday");
+                    break;
+                case "wed":
+                    Console.WriteLine("Wednesday");
+                    break;
+                case "thu":
+                    Console.WriteLine("Thursday");
+                    break;
+                case "fri":
+                    Console.WriteLine("Friday");
+                    break;
+                case "sat":
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine($"{day}은(는) 요일이 아닙니다.");
+                    break;
+
+
+
+            }
+
+
+        }
+    }
+}
