32db0ae856d77982cffc8e5ea1945a304c78722f
Rased Jung <rased0727@gmail.com>
2022-07-07 15:41:02 +0900

    220707 1540

---
+++ Chapter04/Chapter04/Chapter5/For_NestedFor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter5
+{
+    class For_NestedFor
+    {
+        static void Main(string[] args)
+        {
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Chapter04/Chapter04/Chapter5/While_Dowhile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter5
+{
+    class While_Dowhile
+    {
+        static void Main(string[] args)
+        {
+            {
+                int i = 10;
+                while (i > 00)
+                {
+                    Console.WriteLine($"i : {i--}");
+                }
+            }
+            {
+                int i = 10;
+
+                do
+                {
+                    Console.WriteLine("a) i : {0}", i--);
+                }
+                while (i > 0);
+
+                do
+                {
+                    Console.WriteLine("b) i : {0}", i--);
+                }
+                while (i > 0);
+            }
+        }
+    }
+}
