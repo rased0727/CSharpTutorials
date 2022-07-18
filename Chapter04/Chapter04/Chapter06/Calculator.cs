2a16906a8fe8dd5c26d2b86567733e9accd92519
Rased Jung <rased0727@gmail.com>
2022-07-18 15:34:40 +0900

    220718 1534

---
+++ Chapter04/Chapter04/Chapter06/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter06
+{
+    class Calculator
+    {
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+        static void Main(string[] args)
+        {
+            int result = Calculator.Plus(3, 4);
+            Console.WriteLine(result);
+
+            result = Calculator.Minus(3, 4);
+            Console.WriteLine(result);
+        }
+    }
+}
