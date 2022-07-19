30814e72452503dbaa24e098e37cb22032ef8b08
Rased Jung <rased0727@gmail.com>
2022-07-19 15:44:43 +0900

    220719 1544

---
+++ Chapter04/Chapter04/Chapter06/SwapByValRef.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter06
+{
+    class SwapByValRef
+    {
+        public static void Swap1(int a, int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        public static void Swap2(ref int a, ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        static void Main(string[] args)
+        {
+            {
+                int x = 3;
+                int y = 4;
+
+                Console.WriteLine($"x:{x}, y:{y}");
+
+                Swap1(x, y);
+
+                Console.WriteLine($"x:{x}, y:{y}");
+                Console.WriteLine();
+            }
+
+            {
+                int x = 3;
+                int y = 4;
+
+                Console.WriteLine($"x:{x}, y:{y}");
+
+                Swap2(ref x, ref y);
+
+                Console.WriteLine($"x:{x}, y:{y}");
+            }
+        }
+    }
+}
