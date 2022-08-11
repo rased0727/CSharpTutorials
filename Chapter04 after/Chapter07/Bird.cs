7e41d6a8ebd5de0ced43f923b0879fe889ffb056
Rased Jung <rased0727@gmail.com>
2022-08-11 15:38:31 +0900

    a

---
+++ Chapter04 after/Chapter07/Bird.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter07
+{
+    class Bird
+    {
+        protected string Name;
+        public Bird(string Name)
+        {
+            this.Name = Name;
+            Console.WriteLine($"{this.Name}.Bird()");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine($"{Name}.Fly()");
+        }
+
+        
+
+    }
+    class Duck : Bird
+    {
+        public Duck(string Name) : base(Name)
+        {
+            Console.WriteLine($"{this.Name}.Duck()");
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine($"{Name}.MakeSound()");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Bird a = new Bird("a");
+            a.Fly();
+
+            Duck b = new Duck("b");
+            b.Fly();
+            b.MakeSound();
+
+
+        }
+    }
+}
