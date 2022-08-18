4b533a8d7710db8dc91a6933b878658293d37c37
Rased Jung <rased0727@gmail.com>
2022-08-18 15:31:49 +0900

    a

---
+++ Chapter04 after/Chapter07/ArmorSuite.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter07
+{
+    class ArmorSuite
+    {
+        public virtual void Initialize()
+        {
+            Console.WriteLine("Armored");
+        }
+    }
+    class IronMan : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Repulsor Rays Armed");  
+        }
+    }
+    class WarMachine : ArmorSuite
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Console.WriteLine("Double-Barrel Cannons Armed");
+            Console.WriteLine("Micro-Rocket Lunchers Armed");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating ArmorSuite...");
+            ArmorSuite armorsuite = new ArmorSuite();
+            armorsuite.Initialize();
+
+            Console.WriteLine("Creating IronMan...");
+            ArmorSuite ironMan = new IronMan();
+            ironMan.Initialize();
+
+            Console.WriteLine("Creating WarMachine...");
+            ArmorSuite warMachine = new WarMachine();
+            warMachine.Initialize();
+        }
+    }
+}
---
+++ Chapter04 after/Chapter07/ReadOnlyStruct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter07
+{
+    readonly struct RGBColor
+    {
+        public readonly byte R;
+        public readonly byte G;
+        public readonly byte B;
+
+        public RGBColor(byte r, byte g, byte b)
+        {
+            R = r;
+            G = r;
+            B = b;
+        }
+    }
+    class ReadOnlyStruct
+    {
+        static void Main(string[] args)
+        {
+            RGBColor Red = new RGBColor(255, 0, 0);
+            //Red.G = 100; // 컴파일 에러
+        }
+    }
+}
