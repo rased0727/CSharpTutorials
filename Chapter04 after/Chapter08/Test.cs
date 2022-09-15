7d0d05a45849dd201dc3a7c6016a1524e6d63965
Rased Jung <rased0727@gmail.com>
2022-09-15 15:37:43 +0900

    aa

---
+++ Chapter04 after/Chapter08/Test.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter08
+{
+    class Test
+    {
+        // 인코딩 테스트
+    }
+}
---
+++ Chapter04 after/Chapter09/Property.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Chapter09
+{
+    class BirthdayInfo
+    {
+        public string Name { get; set; } = "unknown";
+        public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
+      
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+    class Property
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+
+            birth.Name = "서현";
+            birth.Birthday = new DateTime(1991, 6, 28);
+
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+
+
+        }
+    }
+}
